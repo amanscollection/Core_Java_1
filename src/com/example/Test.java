d9d47ac5b0fb16efeaef7d4f1219cf431df4b9b8
Amandeep <amanscollection@gmail.com>
2025-04-20 04:07:46 +0530

    update files

--- src/com/example/Test.java
+++ src/com/example/Test.java
@@ -3,5 +3,14 @@ package com.example;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello World!!");
+
+        int a=56;
+        System.out.println(a);
+
+        int b=20;
+        System.out.println(b);
+
+        String name = "Amandeep";
+        System.out.println(name);
     }
 }
